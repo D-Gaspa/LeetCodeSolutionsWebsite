8b7d7bc02b30cf35f4e527244c6d568894f15851
Diego Gasparis Escobedo <diego.gaspariseo@udlap.mx>
2024-08-10 19:40:23 -0600

    Refactor views to use TypeScript and script setup syntax
    Updated `Home.vue`, `Login.vue`, and `ProblemList.vue` to use TypeScript and the `<script setup>` syntax for better type safety and conciseness. Adjusted imports and improved form handling in `Login.vue`. Removed unused code in `ProblemList.vue`.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import 'highlight.js/styles/github.css'
 import './assets/theme.css'
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="home">
     <h1 class="main-title">Welcome to LeetCode Solutions</h1>
-    <p class="text-lg"> This is the home page of our LeetCode solutions website.</p>
+    <p class="text-lg">This is the home page of our LeetCode solutions website.</p>
     <button class="btn-special">Get Started</button>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Home'
-}
+<script lang="ts" setup>
 </script>
 
 <style scoped>
